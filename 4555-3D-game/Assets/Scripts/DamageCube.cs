fd2ed7dd700df9c87621525d556de2ccaffaf19e
DRaborn02 <draborn@calstatela.edu>
2025-11-20 06:15:19 -0800

    final tweaks

--- 4555-3D-game/Assets/Scripts/DamageCube.cs
+++ 4555-3D-game/Assets/Scripts/DamageCube.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections;
 
 public class DamageCube : MonoBehaviour
 {
-    [SerializeField] private int damageQuarterHearts = 1; // 1 =  1/4th heart
+    [SerializeField] private int damageQuarterHearts = 1;
+    [SerializeField] private float damageInterval = 1f; // time between damage ticks
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the object has a PlayerHealth component
         PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
         if (playerHealth != null)
         {
-            playerHealth.TakeDamage(damageQuarterHearts);
+            // Start damaging this player as long as they're inside
+            StartCoroutine(DamageOverTime(playerHealth));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            // Stop damaging this player
+            StopCoroutine(DamageOverTime(playerHealth));
+        }
+    }
+
+    private IEnumerator DamageOverTime(PlayerHealth player)
+    {
+        // Initial hit immediately
+        player.TakeDamage(damageQuarterHearts);
+
+        // Continue dealing damage every interval while player remains inside
+        while (true)
+        {
+            yield return new WaitForSeconds(damageInterval);
+
+            // If the player died or was destroyed, stop
+            if (player == null || player.IsDead)
+                yield break;
+
+            player.TakeDamage(damageQuarterHearts);
         }
     }
 }
